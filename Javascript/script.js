44bfa576a70b6d6ab4944465e4e6e65502223d46
Prajwal <prajwalpai21@gmail.com>
2025-02-27 09:22:58 +0530

    New Registration Page for Travel Website

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,41 @@
-function greet(name){
-    alert(("Hello "+name));
+// function greet(name){
+//     alert(("Hello "+name));
+// }
+// greet("Prajwal");
+
+const { Button } = require("bootstrap");
+
+// let interestsNews = [
+//     {sports:"Cricket,Football,Volleyball"},
+//     {politics:"BJP,Congress,JDS"},
+//     {entertainment:"Movies,TV Shows,Cartoons"}
+// ]
+
+// let outputResult = interestsNews.filter(output => output.sports !== undefined);
+// console.log(outputResult);
+
+let person = {
+    name: 'Alice',
+    age:25,
+    greet: function(){
+        console.log("Hello");
+    }
+};
+
+document.write(person.name+"<br>");
+// window.alert(person.name);
+person.greet();
+
+let fruits = ["Apple","Banana"];
+document.write(fruits+"<br>");
+fruits.push("GRAPES");
+document.write(fruits);
+
+    
+function test(){
+    var testVar = 10;
+    document.write(testVar);
 }
-greet("Prajwal");
 
-let interestsNews = [
-    {sports:"Cricket,Football,Volleyball"},
-    {politics:"BJP,Congress,JDS"},
-    {entertainment:"Movies,TV Shows,Cartoons"}
-]
-
-let outputResult = interestsNews.filter(output => output.sports !== undefined);
-console.log(outputResult);
+test();
+document.write(testVar);
