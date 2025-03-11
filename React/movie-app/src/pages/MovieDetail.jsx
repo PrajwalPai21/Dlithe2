2946fa60d43d306dcd406c4b9b4f2486b2f842f0
Prajwal Pai <prajwalpai21@gmail.com>
2025-03-11 09:53:10 +0530

    Update MovieDetail.jsx

--- React/movie-app/src/pages/MovieDetail.jsx
+++ React/movie-app/src/pages/MovieDetail.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const API_KEY = "YOUR_TMDB_API_KEY";
+const API_KEY = "APIKEYHERE";
 const API_URL = "https://api.themoviedb.org/3/movie/";
 
 function MovieDetail() {
