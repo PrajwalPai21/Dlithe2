c812de248dc6c1ccd9411b40423abc578873e13b
Prajwal <prajwalpai21@gmail.com>
2025-03-12 19:01:14 +0530

    pushed day 12 stuff

---
+++ React/snoozebroswebsite/src/pages/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./main.css";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">SnoozeBros</h2>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
